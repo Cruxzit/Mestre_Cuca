7091e413cb58a05e48b8fe21410261810210969b
Hugo <hugocruxzit@gmail.com>
2025-10-03 22:07:37 +0100

    feat: adiciona React Router e rotas para Home e Receitas

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
-import { useState } from 'react'
-import Header from './components/Header'
-import Home from './Home'
-import './App.css'
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Home from './Home';
+import Recipes from './Recipes';
+import './App.css';
 
 function App() {
   return (
-    <>
+    <Router>
       <Header />
-      <Home />
-    </>
-  )
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/receitas" element={<Recipes />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
